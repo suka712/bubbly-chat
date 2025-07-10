ee10dbd64001b11fc7757f4a8e491e369b6d18a2
Suka <khiemnguyen.hye@gmail.com>
2025-07-10 15:55:13 +0700

    Add receiving and sending message route and controller

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express'
 import User from '../models/user.model.ts'
+import Message from '../models/message.model.ts'
 
 export const getUsersForSideBar = async (req: any, res: Response) => {
     try {
@@ -11,5 +12,33 @@ export const getUsersForSideBar = async (req: any, res: Response) => {
     } catch (error) {
         console.log('💢 Error on getUsersForSideBar controller.')
         res.status(500).json({ error: '💢 Something is broken on our end.' })
+    }
+}
+
+export const getMessages = async (req: any, res: Response) => {
+    try {
+        const { id: chatWithId } = req.params
+        const myId = req.user._id
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: chatWithId },
+                { senderId: chatWithId, receiverId: myId },
+            ],
+        })
+
+        res.status(200).json(messages)
+    } catch (error) {
+        console.log('💢 Error on getMessages controller.')
+        res.status(500).json({ error: '💢 Something is broken on our end.' })
+    }
+}
+
+export const sendMessage = async (req: any, res: Response) => {
+    try {
+        const {text, image} = req.body
+        const { id: receiverId } = req.body
+    } catch (error) {
+        
     }
 }
--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import { protectRoute } from '../middleware/auth.middleware.ts'
-import { getUsersForSideBar } from '../controllers/message.controller.ts'
+import { getUsersForSideBar, getMessages } from '../controllers/message.controller.ts'
 
 const router = express.Router()
 
 router.get('/users', protectRoute, getUsersForSideBar)
+router.get('/:id', protectRoute, getMessages)
+
+router.post('/send/:id', sendMessage)
 
 export default router
