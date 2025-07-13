8422f6cdd5d239269fddbe691866c62a697996f7
Suka <khiemnguyen.hye@gmail.com>
2025-07-14 02:05:50 +0700

    Add all SignUp fields

--- frontend/src/Pages/SignupPage.tsx
+++ frontend/src/Pages/SignupPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useAuthStore } from '../store/useAthStore'
-import { MessageSquare, Rabbit } from 'lucide-react'
+import { MessageSquare, Rabbit, Mail, Lock, Eye, EyeOff } from 'lucide-react'
 
 const SignupPage = () => {
     const [showPassword, setPassword] = useState(false)
@@ -31,19 +31,53 @@ const SignupPage = () => {
                             <p className="text-base-content/60">Get started with your free account</p>
                         </div>
                     </div>
-                    {/* FORM */}
+                    {/* -------------------------Form------------------------- */}
                     <form onSubmit={handleSubmit} className="space-y-6">
+                        {/* -------------------------Username------------------------- */}
                         <div className="form-control">
-                            <label className="label">
+                            <label className="label mb-2">
                                 <span className="label-text font-medium">Full Name</span>
                             </label>
                             <div className="relative">
                                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                                     <div className="absolute inset-y-0 left-0 pl-3 flex items-center z-10">
-                                        <Rabbit className="size-6 text-gray-500" style={{strokeWidth: 1}}/>
+                                        <Rabbit className="size-6" style={{ strokeWidth: 1 }} />
                                     </div>
                                 </div>
                                 <input type="text" className={`input input-bordered w-full pl-10`} placeholder="Bruce Wayne" value={formData.username} onChange={(e) => setFormData({ ...formData, username: e.target.value })} />
+                            </div>
+                        </div>
+                        {/* -------------------------Email------------------------- */}
+                        <div className="form-control mb-4">
+                            <label className="label mb-2">
+                                <span className="label-text font-medium">Email</span>
+                            </label>
+                            <div className="relative">
+                                <div className="absolute inset-y-0 left-0 pl-3 flex items-center z-10">
+                                    <Mail className="size-6" style={{ strokeWidth: 1 }} />
+                                </div>
+                                <input type="email" className={`input input-bordered w-full pl-10`} placeholder="you@example.com" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
+                            </div>
+                        </div>
+                        {/* -------------------------Password------------------------- */}
+                        <div className="form-control mb-4">
+                            <label className="label mb-2">
+                                <span className="label-text font-medium">Password</span>
+                            </label>
+                            <div className="relative">
+                                <div className="absolute inset-y-0 left-0 pl-3 flex items-center z-10">
+                                    <Lock className="size-6" style={{ strokeWidth: 1 }} />
+                                </div>
+                                <input
+                                    type={showPassword ? 'text' : 'password'}
+                                    className={`input input-bordered w-full pl-10`}
+                                    placeholder="••••••••"
+                                    value={formData.password}
+                                    onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                                />
+                                <button type="button" className="absolute inset-y-0 right-0 pr-3 flex items-center" onClick={() => setShowPassword(!showPassword)}>
+                                    {showPassword ? <EyeOff className="size-5 text-base-content/40" /> : <Eye className="size-5 text-base-content/40" />}
+                                </button>
                             </div>
                         </div>
                     </form>
