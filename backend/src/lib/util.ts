d8b5443872843484511c79be0b35a53ef1d11b9f
khiem <khiemnguyen.hye@gmail.com>
2025-11-29 11:31:02 +0700

    Add trailing newline to util.ts.

--- backend/src/lib/util.ts
+++ backend/src/lib/util.ts
@@ -3,20 +3,20 @@ import type { Response } from 'express'
 import { Types } from 'mongoose'
 
 export const generateTokenAndSetCookie = (userId: Types.ObjectId, res: Response) => {
-    if (!process.env.JWT_SECRET) {
-        throw new Error('❌ JWT_SECRET is not defined!')
-    }
+  if (!process.env.JWT_SECRET) {
+    throw new Error('❌ JWT_SECRET is not defined!')
+  }
 
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: '7d',
-    })
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '7d',
+  })
 
-    res.cookie('jwt', token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000, // ✏️ In Milisecond
-        httpOnly: true, 
-        sameSite: 'strict',
-        secure: process.env.NODE_ENV !== 'development',
-    })
-    
-    return token
+  res.cookie('jwt', token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // ✏️ In Milisecond
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: process.env.NODE_ENV !== 'development',
+  })
+
+  return token
 }
