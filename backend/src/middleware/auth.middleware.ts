4ca1742a1eb5120c171bf3f85fee41d6f5c3bf10
khiem <khiemnguyen.hye@gmail.com>
2025-12-02 15:06:52 +0700

    Style: Enforce double quotes for strings.

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,31 +1,35 @@
-import * as jwt from 'jsonwebtoken'
-import User from '../models/user.model'
-import type { Response, NextFunction } from 'express'
+import * as jwt from "jsonwebtoken";
+import User from "../models/user.model";
+import type { Response, NextFunction } from "express";
 
-export const protectRoute = async (req: any, res: Response, next: NextFunction) => {
+export const protectRoute = async (
+  req: any,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
-    const token = req.cookies.jwt
+    const token = req.cookies.jwt;
     if (!token) {
-      res.status(401).json({ message: 'Unauthorized.' })
-      return
+      res.status(401).json({ message: "Unauthorized." });
+      return;
     }
 
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET!)
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET!);
     if (!decodedToken) {
-      res.status(401).json({ message: 'Unauthorized.' })
-      return
+      res.status(401).json({ message: "Unauthorized." });
+      return;
     }
 
-    if (typeof decodedToken !== 'object' || !decodedToken?.userId) {
-      res.status(401).json({ message: 'Unauthorized: Invalid token payload' })
-      return
+    if (typeof decodedToken !== "object" || !decodedToken?.userId) {
+      res.status(401).json({ message: "Unauthorized: Invalid token payload" });
+      return;
     }
 
-    const user = await User.findById(decodedToken.userId).select('-password')
-    req.user = user
-    next()
+    const user = await User.findById(decodedToken.userId).select("-password");
+    req.user = user;
+    next();
   } catch (error) {
-    console.log('Error in protectRoute middleware:', error)
-    res.status(500).json({ message: 'Something is broken on our end.' })
+    console.log("Error in protectRoute middleware:", error);
+    res.status(500).json({ message: "Something is broken on our end." });
   }
-}
+};
