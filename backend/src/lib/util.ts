525a475d08b35530e9f14f31c588aea62d12348e
khiem <khiemnguyen.hye@gmail.com>
2025-12-04 00:17:01 +0700

    Style: Format

--- backend/src/lib/util.ts
+++ backend/src/lib/util.ts
@@ -1,22 +1,25 @@
-import * as jwt from 'jsonwebtoken'
-import type { Response } from 'express'
-import { Types } from 'mongoose'
+import * as jwt from "jsonwebtoken";
+import type { Response } from "express";
+import { Types } from "mongoose";
 
-export const generateTokenAndSetCookie = (userId: Types.ObjectId, res: Response) => {
+export const generateTokenAndSetCookie = (
+  userId: Types.ObjectId,
+  res: Response,
+) => {
   if (!process.env.JWT_SECRET) {
-    throw new Error('❌ JWT_SECRET is not defined!')
+    throw new Error("❌ JWT_SECRET is not defined!");
   }
 
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: '7d',
-  })
+    expiresIn: "7d",
+  });
 
-  res.cookie('jwt', token, {
+  res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // ✏️ In Milisecond
     httpOnly: true,
-    sameSite: 'strict',
-    secure: process.env.NODE_ENV !== 'development',
-  })
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development",
+  });
 
-  return token
-}
+  return token;
+};
